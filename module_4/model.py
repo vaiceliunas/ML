4238b31ce1564d17992e65d197df2686d54e39e8
Arnas <arnas.vaiceliunas@gmail.com>
2023-01-15 21:03:17 +0200

    module 4
    different printing

--- module_4/model.py
+++ module_4/model.py
@@ -9,4 +9,21 @@ from six.moves import urllib
 import tensorflow._api.v2.feature_column as fc
 import tensorflow as tf
 
+dftrain = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/train.csv')
+dfeval = pd.read_csv('https://storage.googleapis.com/tf-datasets/titanic/eval.csv')
+print(dftrain.head())
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
 
+print(dftrain.head())
+print(y_train)
+
+print(dftrain.describe())
+
+print(dftrain.shape)
+plt.clf()
+#dftrain.age.hist(bins=20)
+dftrain['class'].value_counts().plot(kind='barh')
+
+#plt.plot()
+plt.show()
