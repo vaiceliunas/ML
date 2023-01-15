17c17b109003d8caccfb70e526ae0eda8e42ee7f
Arnas <arnas.vaiceliunas@gmail.com>
2023-01-15 17:59:13 +0200

    module 3
    core algorithms

---
+++ module_3 core algorithms/linearRegression.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+x = [1, 2, 2.5, 4, 5]
+y = [1, 4, 7, 9, 15]
+
+plt.plot(x, y, 'ro')
+plt.axis([0, 6, 0, 20])
+plt.plot(np.unique(x), np.poly1d(np.polyfit(x, y, 1))(np.unique(x)))
+
+plt.show()
